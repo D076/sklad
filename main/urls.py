28154e82fff64f88fe6a61ee80940a565a71f790
D076 <33459398+D076@users.noreply.github.com>
2021-06-01 15:01:39 +0700

    Обновлены страницы справочников

--- main/urls.py
+++ main/urls.py
@@ -4,12 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('index/<int:id>', views.product_list, name='product_list'),
-    # path('products/<int:id>/delete', views.product_delete, name='product_delete'),
-    # path('products/<int:id>/edit', views.product_edit, name='product_edit'),
-    # path('products/<int:id>', views.product_view, name='product_view'),
-    # path('products/create', views.create_item, name='create_item'),
-    # path('search/', views.search_list, name='search'),
     path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout')
+    path('logout/', views.logout_user, name='logout'),
+    path('sprav/', views.sprav, name='sprav'),
+    path('sprav/materials/', views.sprav_mat, name='sprav_mat'),
+    path('sprav/respons_persons/', views.sprav_persons, name='sprav_persons'),
+    path('sprav/org/', views.sprav_org, name='sprav_org'),
+    path('mat_on_sklad/', views.mat_on_skald, name='mat_on_sklad'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 import json
-from .models import Mat
+from .models import Mat, ResponsPerson, OrgList, StorageMat
 # from .forms import ProductForm
 
 # Create your views here.
@@ -13,8 +13,41 @@ from .models import Mat
 def index(request):
     if request.method == 'GET':
         dict = {'mat': Mat.objects.all()}
-        print(dict)
         return render(request, 'main/index.html', dict)
+
+
+@login_required
+def sprav(request):
+    if request.method == 'GET':
+        return render(request, 'main/sprav.html')
+
+
+@login_required
+def sprav_mat(request):
+    if request.method == 'GET':
+        dict = {'mat': Mat.objects.all()}
+        return render(request, 'main/sprav_mat.html', dict)
+
+
+@login_required
+def sprav_persons(request):
+    if request.method == 'GET':
+        dict = {'respons_persons': ResponsPerson.objects.all()}
+        return render(request, 'main/sprav_persons.html', dict)
+
+
+@login_required
+def sprav_org(request):
+    if request.method == 'GET':
+        dict = {'org': OrgList.objects.all()}
+        return render(request, 'main/sprav_org.html', dict)
+
+
+@login_required
+def mat_on_skald(request):
+    if request.method == 'GET':
+        dict = {'mat_on_sklad': StorageMat.objects.all()}
+        return render(request, 'main/mat_on_sklad.html', dict)
 
 
 def login_user(request):
